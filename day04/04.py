454af48e209166ba7c3a620d06a5539a7bcd6faf
himanshusharma <hs0242262@gmail.com>
2024-10-04 16:12:46 +0530

    c2

---
+++ day04/04.py
@@ -0,0 +1,8 @@
+# wap to extract last value from a given list and it should be a string and len more than 3
+
+a=eval(input("enter:"))
+if type(a[-1])==str and len(a[-1])>=3:
+    print("yes")
+
+
+
